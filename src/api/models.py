2354ac3477e10f511f282c17de1d72fe452a6645
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2023-07-27 19:59:21 +0000

    modals changes

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
 
+import enum
+from sqlalchemy import Enum,ForeignKey
+
+class myEnum(enum.Enum):
+    activity = "activity"
+    product = "product"
+    trip = "trip"
+
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    username = db.Column(db.String(80), unique=True,  nullable = False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,22 +23,28 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            "username" : self.username
         }        
       
-class Activities(db.Model):
+class Reviews(db.Model):
+    __tablename__ = "reviews"
     id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey("user.id"))
+    comments_id = db.Column(db.Integer, ForeignKey("comments.id"))
     title = db.Column(db.String(120), nullable=False)
-    type = db.Column(db.String(120), nullable=False)    
+    type = db.Column(db.String(120), nullable=True)    
     author_name = db.Column(db.String(200), nullable=False)    
     description = db.Column(db.String(1000), nullable=False)
-    location = db.Column(db.String(250), nullable=False)
+    location = db.Column(db.String(250), nullable=True)
     publishing_date = db.Column(db.String(10), nullable=False)
-    link= db.Column(db.String(500), nullable=False)
+    link= db.Column(db.String(500), nullable=True)
     price = db.Column(db.String(200), nullable=False, default="0.0")
+    comments = db.Column(db.String(200), nullable=False)
+    status = db.Column(Enum(myEnum))
+
+
     
     def __repr__(self):
-        return f'<Activities {self.id}>'
+        return f'<Reviews {self.id}>'
       
     def serialize(self):
        return {
@@ -43,53 +57,75 @@ class Activities(db.Model):
             "publishing_date": self.publishing_date,
             "link": self.link,
             "price": self.price,
+            "comments" : self.comments,
+
         }
+    
+class Comments(db.Model):
+     __tablename__ = "comments"
+     id = db.Column(db.Integer, primary_key=True, nullable=False)
+     description = db.Column(db.String(1000), nullable=False)
+     user_id = db.Column(db.Integer, ForeignKey("user.id"))
+     review_id = db.Column(db.Integer, ForeignKey("reviews.id"))
 
-class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    author_name = db.Column(db.String(200), nullable=False)    
-    publishing_date = db.Column(db.String(10), nullable=False)
-    title = db.Column(db.String(120), unique=True, nullable=False)
-    description = db.Column(db.String(1000), unique=True, nullable=False)
-    image = db.Column(db.String(120), nullable=False)
-    price = db.Column(db.Integer, nullable=False)
+     def __repr__(self):
+        return f'<Comments {self.id}>'
 
-    def __repr__(self):
-        return '<Product %r>' % self.title
-
-    def serialize(self):
+     def serialize(self):
         return {
             "id": self.id,
-            "title": self.title,
-            "description": self.description,            
-            "price": self.price,          
-            "image": self.image,
-            "author_name" : self.author_name,
-            "publishing_date" : self.publishing_date
-        }
+            "description" : self.description
+        }        
 
+     
 
-class Trips(db.Model):
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    title = db.Column(db.String(120), nullable=False)
-    author_name = db.Column(db.String(200), nullable=False)    
-    description = db.Column(db.String(1000), nullable=False)
-    location = db.Column(db.String(250), nullable=False)
-    publishing_date = db.Column(db.String(10), nullable=False)
-    link= db.Column(db.String(500), nullable=False)
-    price = db.Column(db.String(200), nullable=False, default="0.0")
     
-    def __repr__(self):
-        return f'<Trips {self.id}>'
+
+# class Product(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     author_name = db.Column(db.String(200), nullable=False)    
+#     publishing_date = db.Column(db.String(10), nullable=False)
+#     title = db.Column(db.String(120), unique=True, nullable=False)
+#     description = db.Column(db.String(1000), unique=True, nullable=False)
+#     image = db.Column(db.String(120), nullable=False)
+#     price = db.Column(db.Integer, nullable=False)
+
+#     def __repr__(self):
+#         return '<Product %r>' % self.title
+
+#     def serialize(self):
+#         return {
+#             "id": self.id,
+#             "title": self.title,
+#             "description": self.description,            
+#             "price": self.price,          
+#             "image": self.image,
+#             "author_name" : self.author_name,
+#             "publishing_date" : self.publishing_date
+#         }
+
+
+# class Trips(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, nullable=False)
+#     title = db.Column(db.String(120), nullable=False)
+#     author_name = db.Column(db.String(200), nullable=False)    
+#     description = db.Column(db.String(1000), nullable=False)
+#     location = db.Column(db.String(250), nullable=False)
+#     publishing_date = db.Column(db.String(10), nullable=False)
+#     link= db.Column(db.String(500), nullable=False)
+#     price = db.Column(db.String(200), nullable=False, default="0.0")
+    
+#     def __repr__(self):
+#         return f'<Trips {self.id}>'
       
-    def serialize(self):
-       return {
-            "id": self.id,
-            "title": self.title,
-            "author_name":self.author_name,
-            "description": self.description,
-            "location":self.location,
-            "publishing_date": self.publishing_date,
-            "link": self.link,
-            "price": self.price,
-        }
+#     def serialize(self):
+#        return {
+#             "id": self.id,
+#             "title": self.title,
+#             "author_name":self.author_name,
+#             "description": self.description,
+#             "location":self.location,
+#             "publishing_date": self.publishing_date,
+#             "link": self.link,
+#             "price": self.price,
+#         }
