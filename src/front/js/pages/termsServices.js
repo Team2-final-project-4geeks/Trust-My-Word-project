b23c7393b546b628efc45c1ac133b0f4e1b82109
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2023-08-05 17:34:06 +0000

    background footer, terms finished

--- src/front/js/pages/termsServices.js
+++ src/front/js/pages/termsServices.js
@@ -7,32 +7,33 @@ export const TermsServices = () =>{
 
 <div className="privacy_policy">
     <p>Sample Website Terms and Conditions Template
-    Please read these terms and conditions ("terms and conditions", "terms") carefully
-    before using [website URL] website (“website”, "service") operated by [company name]
-    ("us", 'we", "our").
+    Please read these terms and conditions carefully
+    before using <strong>TrustMyWord</strong> website operated by TrustMyWord Company
+    
     Conditions of use
     By using this website, you certify that you have read and reviewed this Agreement and
     that you agree to comply with its terms. If you do not want to be bound by the terms of
-    this Agreement, you are advised to stop using the website accordingly. [company name]
+    this Agreement, you are advised to stop using the website accordingly. TrustMyWord Company
     only grants use and access of this website, its products, and its services to those who
     have accepted its terms.
+
     Privacy policy
-    Before you continue using our website, we advise you to read our privacy policy [link to
-    privacy policy] regarding our user data collection. It will help you better understand our
+
+    Before you continue using our website, we advise you to read our privacy <a class="nav-link text-light" href="#" onClick={() => navigate("/privacy-policy")}>Privacy Policy </a> regarding our user data collection. It will help you better understand our
     practices.
     Age restriction
-    You must be at least 18 (eighteen) years of age before you can use this website. By
+    You must be at least 18 years of age before you can use this website. By
     using this website, you warrant that you are at least 18 years of age and you may
-    legally adhere to this Agreement. [company name] assumes no responsibility for
+    legally adhere to this Agreement. TrustMyWord Company assumes no responsibility for
     liabilities related to age misrepresentation.
     Intellectual property
     You agree that all materials, products, and services provided on this website are the
-    property of [company name], its affiliates, directors, officers, employees, agents,
+    property of TrustMyWord Company, its affiliates, directors, officers, employees, agents,
     suppliers, or licensors including all copyrights, trade secrets, trademarks, patents, and
     other intellectual property. You also agree that you will not reproduce or redistribute the
-    [company name]’s intellectual property in any way, including electronic, digital, or new
+    TrustMyWord Company’s intellectual property in any way, including electronic, digital, or new
     trademark registrations.
-    You grant [company name] a royalty-free and non-exclusive license to display, use,
+    You grant TrustMyWord Company a royalty-free and non-exclusive license to display, use,
     copy, transmit, and broadcast the content you upload and publish. For issues regarding
     intellectual property claims, you should contact the company in order to come to an
     agreement.
@@ -47,24 +48,26 @@ export const TermsServices = () =>{
     We reserve all rights to terminate accounts, edit or remove content and cancel orders at
     our sole discretion.
     Applicable law
-    By using this website, you agree that the laws of the [your location], without regard to
+    By using this website, you agree that the laws of the UE, without regard to
     principles of conflict laws, will govern these terms and conditions, or any dispute of any
-    sort that might come between [company name] and you, or its business partners and
+    sort that might come between TrustMyWord Company and you, or its business partners and
     associates.
+
     Disputes
+
     Any dispute related in any way to your use of this website or to products you purchase
-    from us shall be arbitrated by state or federal court [your location] and you consent to
+    from us shall be arbitrated by state or federal court UE and you consent to
     exclusive jurisdiction and venue of such courts.
     Indemnification
-    You agree to indemnify [company name] and its affiliates and hold [company name]
+    You agree to indemnify TrustMyWord Company and its affiliates and hold TrustMyWord Company
     harmless against legal claims and demands that may arise from your use or misuse of
     our services. We reserve the right to select our own legal counsel.
     Limitation on liability
-    [company name] is not liable for any damages that may occur to you as a result of your
+    TrustMyWord Company is not liable for any damages that may occur to you as a result of your
     misuse of our website.
-    [company name] reserves the right to edit, modify, and change this Agreement at any
+    TrustMyWord Company reserves the right to edit, modify, and change this Agreement at any
     time. We shall let our users know of these changes through electronic mail. This
-    Agreement is an understanding between [company name] and the user, and this
+    Agreement is an understanding between TrustMyWord Company and the user, and this
     supersedes and replaces all prior agreements regarding the use of this website.
     </p>
 </div>
