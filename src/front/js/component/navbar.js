e888a69966909f9f806b61af31f952af0b5974b0
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2023-08-07 08:45:58 +0000

    put logout, some changes in css in navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -19,7 +19,10 @@ export const Navbar = () => {
 	useEffect(() => {
 		localStorage.setItem("favourites", JSON.stringify(store.favourite));
 	}, [store.favourite]);
-	
+
+	const logOut = () => {
+		localStorage.removeItem('jwt-token')
+	}
 	
     return (
         <nav className="navbar">
@@ -27,38 +30,41 @@ export const Navbar = () => {
 				<Link to="/">
 					<img src="https://sd.keepcalms.com/i/keep-calm-and-trust-my-words.png" width="150" alt="..." height="80" bg="light" className="d-inline-block align-text-top"/>                  
                 </Link>		
-				<div class="navbar-items">
+				<div class="navbar-items d-flex-block">
 						<div className="navbar-links">
 							<ul class="d-flex align-items-end">
 								<li class="nav-item">
 									<a class="nav-link text-light" href="#" onClick={() => navigate("/")}>Home</a>
 								</li>
 								<li class="nav-item">
+									<a class="nav-link text-light" href="#" onClick={() => navigate("/activities")}>Activities</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link  text-light" href="#" onClick={() => navigate("/products")}>Products</a>
+								</li>
+								<li class="nav-item">
+									<a class="nav-link text-light" href="#" onClick={() => navigate("/trips")}>Trips</a>
+								</li>
+								<li class="nav-item">
 									<a class="nav-link text-light" href="#" onClick={() => navigate("/create-user")}>Create User </a>
 								</li>
 								<li class="nav-item">
 									<a class="nav-link text-light" href="#" onClick={() => navigate("/login")}>Login</a>
-								</li>	
-								<li class="nav-item">
-									<a class="nav-link text-light" href="#" onClick={() => navigate("/trips")}>Trips</a>
-								</li>	
-								<li class="nav-item">
-									<a class="nav-link text-light" href="#" onClick={() => navigate("/activities")}>Activities</a>
-								</li>	
-								<li class="nav-item">
-									<a class="nav-link  text-light" href="#" onClick={() => navigate("/products")}>Products</a>
 								</li>
+								<li class="nav-item">
+									<a class="nav-link text-light" href="#" onClick={logOut}>Logout</a>
+								</li>							
 							</ul>
 						</div>				
-						<div className="btn-group mx-5">
-							<button type="button" className="btn btn-success dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" id="dropdownMenuClickableInside" aria-expanded="false">
+						<div className="btn-group-favourites">
+							<button type="button" className="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" id="dropdownMenuClickableInside" aria-expanded="false">
 								Favourites <span className="p-1 text-secondary text-center text-white">{store.favourite.length}</span>
 							</button>
 							<ul className="dropdown-menu dropdown-menu-end dropdown-menu-lg-start" aria-labelledby="dropdownMenuClickableInside">
 								{store.favourite.map((fav, index) => {
 									return(
 										<li key={index}>
-											<a className="dropdown-item d-flex justify-content-between ps-2 pe-2" href="#">
+											<a className="dropdown-item d-flex text-black justify-content-between ps-2 pe-2" href="#">
 												{fav}
 												<i className="fas fa-trash pt-1"
 													onClick={() => {
