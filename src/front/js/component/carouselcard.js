aacad70481352afcf63851818188ab7fa2bb5e77
LucyMacko <118434024+LucyMacko@users.noreply.github.com>
2023-09-05 16:01:10 +0000

    activities adjusted

--- src/front/js/component/carouselcard.js
+++ src/front/js/component/carouselcard.js
@@ -39,14 +39,14 @@ const CarouselCard = (props) =>{
 
   return(
     <div className="card mb-5" id="carouselBody">
-      <div className="card-body">
+      <div className="card-body" id="bodyComment">
         <div className="d-flex flex-row justify-content-between mt-1">
           <img className="me-2" id="fotoCarouselCard" src={props.image} alt="Photo provided by the User"></img>
-          <h5 className="card-title">{props.author}</h5>
+          <h5 className="card-title me-2">{props.author}</h5>
           {props.userLogged == props.authorId ? <ImBin className="deleteIcon mt-1" onClick={() => deleteComment(props.id)}/> : ""}
         </div>
-        <p className="card-text mt-4">{props.description}</p>
-        <p className="card-text mt-4"><small className="text-muted publishing-date">{props.date}</small></p>
+        <p className="card-text mt-3 mb-1" id="commentText">{props.description}</p>
+        <p className="card-text mt-1"><small className="text-muted publishing-date">{props.date}</small></p>
       </div>  
     </div>
   )
