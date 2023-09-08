a22b2e69ce2de5d51aa765736bceb31cce54aead
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2023-09-08 18:17:47 +0000

    working on navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -137,58 +137,62 @@ export const Navbar = () => {
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div class="container-fluid">
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon">Menu</span>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation" >
+    <span class="navbar-toggler-icon border-0" id="hamburguer">
+      <div class="bar1"></div>
+      <div class="bar2"></div>
+      <div class="bar3"></div>
+    </span>
   </button>
   <a class="navbar-brand" href="/"><img src="https://i.ibb.co/8m2mpN3/0d3b546942f94de196812ac8af0bf4d9-fotor-bg-remover-20230809143940.png" alt="0d3b546942f94de196812ac8af0bf4d9" border="0" id="imageBrand" /></a>
   <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <ul class="navbar-nav me-auto mt-0 mb-2 mb-lg-0" id="navbarContent">
       <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active text-warning" aria-current="page" href="#">Home</a>
       </li>
-      <li class="nav-item">
-        <button class="btn dropdown text-light" id="category" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <li class="nav-item pl-0">
+        <button class="btn dropdown text-warning ml-0" id="category" type="button" data-bs-toggle="dropdown" aria-expanded="false">
           Category
         </button>
           <ul class="dropdown-menu" id="scroll-down">
             <li>
-              <a className="nav-link text-light" id="scroll-down2" href="#" onClick={() => navigate("/activities")}>
+              <a className="nav-link text-warning" id="scroll-down2" href="#" onClick={() => navigate("/activities")}>
               Activities
               </a>
             </li>
             <li>
-              <a className="nav-link text-light" id="scroll-down2" href="#" onClick={() => navigate("/products")}>
+              <a className="nav-link text-warning" id="scroll-down2" href="#" onClick={() => navigate("/products")}>
               Products
               </a>
             </li>
             <li>
-              <a className="nav-link text-light" id="scroll-down2" href="#" onClick={() => navigate("/trips")}>
+              <a className="nav-link text-warning" id="scroll-down2" href="#" onClick={() => navigate("/trips")}>
               Trips
               </a>
             </li>
           </ul>
       </li>
-      <li class="nav-item">
+      <li class="nav-item" id="userNavbar">
         {token ? ( 
             <div className="token">
               <div className="nav-item">
-                <a className="nav-link text-light" href="#" onClick={() => navigate("/user-page")}>
+                <a className="nav-link text-warning" href="#" onClick={() => navigate("/user-page")}>
                   User Page
                 </a>
               </div>
               <div className="nav-item">
-                <a className="nav-link text-light" href="#" onClick={() => navigate("/create-review")}>
+                <a className="nav-link text-warning" href="#" onClick={() => navigate("/create-review")}>
                   Create Review
                 </a>
               </div>
               <div className="nav-item">
-                <a className="nav-link text-light" href="#" onClick={logOut}>
+                <a className="nav-link text-warning" href="#" onClick={logOut}>
                   Logout
                 </a>
               </div>
               <div className="btn-group" id="favourites">
                 <button type="button" className="btn-navbar dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" id="dropdownMenuClickableInside" aria-expanded="false">
-                    Favourites <span className="p-1 text-center text-white">{(store.favourite && store.favourite!=null && store.favourite!=undefined)? store.favourite.length:"0"}</span>
+                    Favourites <span className="p-1 text-center text-warning">{(store.favourite && store.favourite!=null && store.favourite!=undefined)? store.favourite.length:"0"}</span>
                 </button>
                 {store.favourite && store.favourite.length > 0 ? (
                     <ul className="dropdown-menu dropdown-menu-lg-end" aria-labelledby="dropdownMenuClickableInside">
@@ -218,7 +222,7 @@ export const Navbar = () => {
             </div>
           ) : (
             <div className="nav-item">
-              <a className="nav-link text-light" href="#" onClick={() => navigate("/login")}>
+              <a className="nav-link text-warning" href="#" onClick={() => navigate("/login")}>
                 Login
               </a>
             </div>
